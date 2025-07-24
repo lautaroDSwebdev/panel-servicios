642b3b5f2ada0420adfc1d5ca98f5e6e7b01f6be
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-07-24 13:55:01 -0300

    fix_ text center footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -15,7 +15,7 @@ export const Footer = () => {
           ))}
         </div> */}
         {/* Columna derecha: redes sociales */}
-        <div className="flex-1 flex flex-col gap-2 items-end">
+        <div className="flex-1 flex flex-col gap-2 items-center">
           <h4 className="font-bold mb-2 text-[#fff] dark:text-[#000]">Redes Sociales</h4>
           <div className="flex flex-col gap-1">
             {socialLinks.map((e) => (
