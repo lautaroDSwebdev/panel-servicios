a5ede9bc07536ac0d4a78ab991dacb3c6701c5ef
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-07-24 14:08:28 -0300

    fix: acentos

--- src/app/_mock/dataEs.ts
+++ src/app/_mock/dataEs.ts
@@ -3,7 +3,7 @@ export const dataEs = {
         tituloNavbar: "Barra lateral de servicios",
         tituloPagina: "Muestras de los servicios",
         tituloCarrusel: "Carrusel de imágenes o testimonios",
-        tituloMapa: "Mapa de google con ubicacion integrada",
+        tituloMapa: "Mapa de Google con ubicación integrada",
         tituloCatalogoProds: "Catalogo de productos de muestra",
         navCarrusel: "Carrusel",
         navMapa: "Mapa",
