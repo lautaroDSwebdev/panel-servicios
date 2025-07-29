b2e387c9f80aad390452a601370e48c91577b62a
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-07-29 17:31:47 -0300

    fix: image carrusel

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -162,7 +162,7 @@ function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
       aria-roledescription="slide"
       data-slot="carousel-item"
       className={cn(
-        "min-w-0 shrink-0 grow-0 basis-[80%] sm:basis-[90%] xl:basis-[50%]",
+        "min-w-0 shrink-0 grow-0 basis-[100%] sm:basis-[90%] xl:basis-[70%]",
         orientation === "horizontal" ? "pl-4" : "pt-4",
         className
       )}
