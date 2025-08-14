894af2dc7f8639f5b5fc6a4aaf54c28e4434b639
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-08-14 19:04:25 -0300

    fix: prods grid prices

--- src/app/CatalogoProds.tsx
+++ src/app/CatalogoProds.tsx
@@ -17,11 +17,13 @@ export const CatalogoProds = () => {
          
           gap-[2rem] justify-center ">
           {prods.map((e) => (
-            <div key={e.id} className="bg-[#393676] dark:bg-[#e0deff] hover:bg-[#c7c3f3] rounded-3xl p-[1.5rem] flex flex-col  items-center overflow-hidden">
-                <img className="h-auto w-[15rem] rounded-3xl hover: relative opacity-85 cursor-pointer aspect-[1/1]
+            <div key={e.id} className="bg-[#393676] dark:bg-[#e0deff] hover:bg-[#504e7c] 
+            hover:dark:bg-[#bfbce9] 
+            rounded-3xl p-[1.5rem] flex flex-col  items-center overflow-hidden">
+              <img className="h-auto w-[15rem] rounded-3xl hover: relative opacity-85 cursor-pointer aspect-[1/1]
                 hover:opacity-100 transition-all ease-in-out .3s scale-100 hover:scale-105 " src={e.img} alt={e.imgAlt} />
-                <p className="p-[5px] dark:text-[#494747] text-[#fff] text-center">{e.desc}</p>
-                <p className="p-[5px] dark:text-[#000] font-bold text-[#fff] text-[1.3rem]">${e.precio}</p>
+              <p className="p-[5px] dark:text-[#494747] text-[#fff] text-center  overflow-hidden text-ellipsis">{e.desc}</p>
+              <p className="p-[5px] dark:text-[#000]  text-[#fff] text-[1.5rem]">${e.precio}</p>
             </div>
           ))}
         </article>
@@ -29,23 +31,3 @@ export const CatalogoProds = () => {
     </div>
   );
 };
-// .g-div_imgProd {
-//   display: flex;
-//   justify-content: center;
-//   overflow: hidden;
-//   img {
-//     position: relative;
-//     height: 11rem;
-//     width: 14rem;
-//     cursor: pointer;
-//     opacity: 0.6;
-//     transition: all 0.5s ease-out;
-//     z-index: 300;
-//     &:hover {
-//       transition: all 0.5s ease-out;
-//       rotate: 2deg;
-//       opacity: 1;
-//       scale: 1.2;
-//     }
-//   }
-// }
